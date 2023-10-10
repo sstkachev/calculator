1a1d0c0a3658805d24ab92143775428b33de757d
sstkachev <sstkachev@gmail.com>
2023-10-10 23:51:24 +0300

    added UI with all buttons, functions for some operators

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final buttonColor = MaterialStateProperty.all(Colors.grey.shade200);
+  final numberTextColor = Colors.grey.shade500;
+  final operationTextColor = Colors.blue.shade700;
+  String result = '';
+  bool hasBracket = false;
 
   @override
   Widget build(BuildContext context) {
@@ -30,19 +41,262 @@ class MainScreen extends StatelessWidget {
         title: const Text('CALCULATOR'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          SizedBox(
-            height: 40,
+          const SizedBox(
+            height: 32,
           ),
-          Text('numbers'),
-          SizedBox(
-            height: 24,
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Container(
+              alignment: Alignment.topRight,
+              child: Text(
+                result,
+                style: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                maxLines: 3,
+                //textDirection: TextDirection.rtl,
+              ),
+            ),
           ),
-          Divider(),
-          ButtonCircle(
-            buttonText: '1',
-            buttonFunc: () {},
+          const SizedBox(
+            height: 32,
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ButtonCircle(
+                buttonText: 'C',
+                buttonFunc: () {
+                  setState(() {
+                    result = '';
+                    hasBracket = false;
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '()',
+                buttonFunc: () {
+                  setState(() {
+                    if (hasBracket) {
+                      result = '$result)';
+                      hasBracket = !hasBracket;
+                    } else {
+                      result = result + '(';
+                      hasBracket = !hasBracket;
+                    }
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '%',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '%';
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '/',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '/';
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ButtonCircle(
+                buttonText: '7',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '7';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '8',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '8';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '9',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '9';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '*',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '*';
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ButtonCircle(
+                buttonText: '4',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '4';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '5',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '5';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '6',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '6';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '-',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '-';
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ButtonCircle(
+                buttonText: '1',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '1';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '2',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '2';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '3',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '3';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '+',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '+';
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ButtonCircle(
+                buttonText: '+/-',
+                buttonFunc: () {},
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '0',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '0';
+                  });
+                },
+                buttonTextColor: numberTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '.',
+                buttonFunc: () {
+                  setState(() {
+                    result = result + '.';
+                  });
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: buttonColor,
+              ),
+              ButtonCircle(
+                buttonText: '=',
+                buttonFunc: () {
+                  setState(() {});
+                },
+                buttonTextColor: operationTextColor,
+                buttonColor: MaterialStateProperty.all(
+                  Colors.blue.shade200,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 12,
+          )
         ],
       ),
     );
